9e24496144b324923c0b8a7c8d7c5d438534855e
Dr.Epico <adadashipour@gmail.com>
2024-06-10 22:55:13 +0330

    Cut repetition from addInfoPane()

--- src/pageElements.js
+++ src/pageElements.js
@@ -46,64 +46,28 @@ function addSearchBox(parent){
     });
 }
 
-export function addInfoPane(parent){
+export function addInfoPane(parent) {
     let detailsPane = document.createElement('div');
     detailsPane.id = "details";
+
     let info = document.createElement('div');
     info.id = "info";
+
+    let infoIds = ['wind', 'humidity', 'precipitation', 'pressure', 'feelsLike', 'uv', 'astro1', 'astro2', 'astro3'];
+    infoIds.forEach(id => {
+        let div = document.createElement('div');
+        div.id = id;
+        info.appendChild(div);
+    });
+
     let hourly = document.createElement('div');
     hourly.id = "hourly";
 
-    let wind = document.createElement('div');
-    let humidity = document.createElement('div');
-    let precipitation = document.createElement('div');
-    let pressure= document.createElement('div');
-    let feelsLike = document.createElement('div');
-    let uv = document.createElement('div');
-    let astro1 = document.createElement('div');
-    let astro2 = document.createElement('div');
-    let astro3 = document.createElement('div');
-    info.appendChild(wind);
-    info.appendChild(humidity);
-    info.appendChild(precipitation);
-    info.appendChild(pressure);
-    info.appendChild(feelsLike);
-    info.appendChild(uv);
-    info.appendChild(astro1);
-    info.appendChild(astro2);
-    info.appendChild(astro3);
-    
-    //9 children 
-    let hour1 = document.createElement('div');
-    hour1.classList.add('hour');
-    let hour2 = document.createElement('div');
-    hour2.classList.add('hour');
-    let hour3 = document.createElement('div');
-    hour3.classList.add('hour');
-    let hour4 = document.createElement('div');
-    hour4.classList.add('hour');
-    let hour5 = document.createElement('div');
-    hour5.classList.add('hour');
-    let hour6 = document.createElement('div');
-    hour6.classList.add('hour');
-    let hour7 = document.createElement('div');
-    hour7.classList.add('hour');
-    let hour8 = document.createElement('div');
-    hour8.classList.add('hour');
-    let hour9 = document.createElement('div');
-    hour9.classList.add('hour');
-    let hour10 = document.createElement('div');
-    hour10.classList.add('hour');
-    hourly.appendChild(hour1);
-    hourly.appendChild(hour2);
-    hourly.appendChild(hour3);
-    hourly.appendChild(hour4);
-    hourly.appendChild(hour5);
-    hourly.appendChild(hour6);
-    hourly.appendChild(hour7);
-    hourly.appendChild(hour8);
-    hourly.appendChild(hour9);
-    hourly.appendChild(hour10);
+    for (let i = 1; i <= 10; i++) {
+        let hour = document.createElement('div');
+        hour.classList.add('hour');
+        hourly.appendChild(hour);
+    }
 
     detailsPane.appendChild(info);
     detailsPane.appendChild(hourly);
