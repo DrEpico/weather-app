51ad6a8cabd729778740fc475ea2a208b1600001
Dr.Epico <adadashipour@gmail.com>
2024-06-10 15:02:33 +0330

    Added more relevant data to body

--- src/index.js
+++ src/index.js
@@ -9,29 +9,33 @@ body.appendChild(searchDiv);
 
 searchButton.addEventListener('click', function(){
     searchContent = searchBox.value.trim();
-    getCats(searchContent);
+    getCurrentWeather(searchContent);
 });
 
-let img = document.createElement('img');
-img.setAttribute('src', '#');
+// let img = document.createElement('span');
+// img.setAttribute('src', '#');
 
-async function getCats(searchContent){
+async function getCurrentWeather(searchContent){
     try {
-        const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=11111111111111111&q=${searchContent}`, {mode: 'cors'});
-        const catData = await response.json();
-        if (catData.data && catData.data.images && catData.data.images.original.url) {
-            img.src = catData.data.images.original.url;
-            console.log(catData.data.images.original.url);
-            body.appendChild(img);
+        const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=195157837b034a1d883103942240706&q=${searchContent}&aqi=no`, {mode: 'cors'});
+        const weatherData = await response.json();//parse to js object from json
+        if (weatherData) {
+            let currentConditionText = weatherData.current.condition.text;
+            let currentConditionElement = document.createElement('span');
+            currentConditionElement.textContent = currentConditionText;
+            body.appendChild(currentConditionElement);
         } else {
             // Handle case where no GIFs are found
-            img.src = 'https://via.placeholder.com/150?text=No+GIF+Found';
-            console.log('No GIF found for the search keyword.');
+            // img.src = 'https://via.placeholder.com/150?text=No+GIF+Found';
+            console.log('No data found for the search keyword.');
+            currentConditionElement.textContent = "No weather data was found"
+            body.appendChild(currentConditionElement);
         }
     } catch (error) {
         // Handle network errors or other fetch-related errors
-        img.src = 'https://via.placeholder.com/150?text=Error';
+        // img.src = 'https://via.placeholder.com/150?text=Error';
         console.error('An error occurred:', error);
+        currentConditionElement.textContent = "Network error"
     }
     
 }
